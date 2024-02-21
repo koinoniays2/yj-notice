4f9de6aef1ba1f393c9b6b19e5cf76f4313b0cb5
koinoniays2 <koinonia_ys@naver.com>
2024-02-21 11:37:18 +0900

    .

--- src/api.js
+++ src/api.js
@@ -1,8 +1,8 @@
 export async function apiPostNoticeWrite(formData) {
     try{
         // console.log("API", formData);
-        return await fetch("http://localhost:4000/notice/write", {
-            // 로컬-http://localhost:4000
+        return await fetch("https://server-koinonia.koyeb.app/notice/write", {
+            // 로컬서버접속-http://localhost:4000
             method: "POST",
             headers: {
                 "Content-Type" : "application/json" // 서버에 어떤 데이터로 요청할것인지
